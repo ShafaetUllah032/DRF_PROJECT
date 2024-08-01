6964180f37b5eee56ab4ef71329a2b0a1307b92e
Safaet Ullah <shafetullah02@gmail.com>
2024-08-01 15:38:16 +0600

    drf spectacular started

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-
-
-
 from mptt.models import MPTTModel,TreeForeignKey
 
 
 
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(max_length=100)
-    parent=TreeForeignKey("self",on_delete=models.PROTECT,null=True,blank=True)
-
+    parent=TreeForeignKey("self",on_delete=models.PROTECT,
+                          null=True,blank=True
+                          )
     class MPTTMeta:
         order_insertion_by = ["name"]
 
@@ -29,9 +27,8 @@ class Product(models.Model):
     is_digital=models.BooleanField(default=False)
     brand=models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey(
-        "Category", on_delete=models.SET_NULL, null=True,blank=True
+        "Category", on_delete=models.SET_NULL, 
+        null=True,blank=True
     )
-
-
     def __str__(self):
         return self.name
--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # External Package
     'rest_framework',
+    "drf_spectacular",
     # Internal Apps
     'drfecommerce.product',
     
@@ -127,6 +128,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK={
-    
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",  
+}
+
+
+SPECTACULAR_SETTINGS={
+    "TITLE": "Django DRF Ecommerce",
+
 }
     
